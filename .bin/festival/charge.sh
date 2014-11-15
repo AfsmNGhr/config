6429ab222b4264f11ff5d8f1831f9a19056ab3b9
Afay Zangetsu <afay.zangetsu@gmail.com>
2014-11-15 22:39:49 +0300

    work on charge speak ...

--- .bin/festival/charge.sh
+++ .bin/festival/charge.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-scripts=/home/afsmnghr/.bin/festival/
+scripts="$HOME/.bin/festival/"
 charge=$(acpi -b | grep -o [0-9]* | sed "2 ! d")
 procent=$(${scripts}pluralform.sh $charge процент процента процентов)
 status=$(acpi -b | cut -d " " -f 3 | cut -d "," -f 1)
-volume=$(amixer | grep -o [0-9]* | sed "5 ! d")
 text=$(echo "Заряд батареи $charge $procent")
+critical="Критический заряд батареи. Требуется подъзарядка"
 
-if test "$status" == 'Discharging';
+if [ "$status" == 'Discharging' ] && [ $charge -gt 5 -a $charge -lt 50 ];
 then
-    amixer -D pulse set Master 30% > /dev/null 2>&1;
     ${scripts}stop_play.sh > /dev/null 2>&1
     ${scripts}say.sh "$text"
-    amixer -D pulse set Master $volume% > /dev/null 2>&1;
+    ${scripts}stop_play.sh > /dev/null 2>&1
+elif [ "$status" == 'Discharging' ] && [ $charge -lt 5 ];
+then
+    ${scripts}stop_play.sh > /dev/null 2>&1
+    ${scripts}say.sh "$critical"
     ${scripts}stop_play.sh > /dev/null 2>&1
 fi
-exit 0
+exit
--- .bin/festival/say.sh
+++ .bin/festival/say.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-set -x
 
-user="afsmnghr"
-data=/home/$user/.bin/festival/data
+data="$HOME/.bin/festival/data"
 ru="(voice_msu_ru_nsh_clunits)"
-n=1
+volume=$(amixer | grep -o [0-9]* | sed "5 ! d")
+i=$(ls -r $data | grep -o [0-9]* | sed "1 ! d")
 
-while test -f $data/saytext_ru_$n.wav;
-do
-    n=$(($n+1))
-    return
-done
+if test -z "$i";
+then i=1;
+elif test -f "$data/saytext_$i.wav";
+then i=$(($i+1));
+fi
 
-echo "$1" | text2wave -o $data/saytext_$n.wav -eval $ru > /dev/null 2>&1
-aplay $data/saytext_$n.wav > /dev/null 2>&1
-# rm -f $data/saytext_$n.wav
+echo "$1" | text2wave -o $data/saytext_$i.wav -eval $ru > /dev/null 2>&1
+amixer set Master 40% > /dev/null 2>&1
+aplay $data/saytext_$i.wav > /dev/null 2>&1
+amixer -D pulse set Master $volume% > /dev/null 2>&1
+rm -f $data/saytext_$i.wav
